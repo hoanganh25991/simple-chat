c717ac9b92f2cc94810d8c4377f90bb453a04657
Lê Hoàng Anh <lehoanganh25991@gmail.com>
2016-03-02 00:52:14 +0700

    fix bug on socket

--- public/client.js
+++ public/client.js
@@ -1,25 +1,16 @@
-//script src="/socket.io/socket.io.js" provide global io
-//socket
-/*global io*/
-var socket = io();
-//form
-// var form = document.querySelector("form");
-var form = $("form");
-//function send_message
-var send_message = function(){
-    //msg from input
-    var input = document.querySelector("input");
-    var msg = input.val();
-    //create li inside ul
-    var ul = document.querySelector("ul");
-    var li = document.createElement("li");
-    //show msg on ul
-    li.innerHTML(msg);
-    ul.appendChild(li);
-    //send msg to server
-    socket.emit("chat_room", msg);
-    //prevent form submit
-    return false;
-}
-//add send_message on form
-form.submit(send_message);
+var socket;
+window.onload = function(){
+	socket = io();
+	$("form").submit(function(){
+		//get text
+		var input = $("#m");
+        var text = input.val();
+        input.val("");
+        socket.emit("chat_room", text);
+        return false;
+	});
+    socket.on("chat_room", function(msg){
+        console.log(msg);
+        $("#messages").append($("<li>").text(msg));
+    });
+};
--- server.js
+++ server.js
@@ -1,32 +1,41 @@
-//import express
 var express = require("express");
-//application-context
 var app = express();
-//http
+
 var http = require("http");
-//server
 var server = http.Server(app);
-//run server
-server.listen(process.env.PORT, process.env.IP, ()=>{
-    console.log("server: running");
-});
-//public folder
-app.use(express.static("public"));
-//handle application request/response
-app.get("/", (req, res)=>{
+
+app.use(express.static('public'));
+server.listen(3000, function(){console.log("server is running");});
+
+/**
+ * socket-io
+ */
+var socketio = require("socket.io");
+var io = socketio(server, {});
+
+/**
+ * handle request
+ */
+app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
-//socketio
-var socket_io = require('socket.io');
-//io
-var io = socket_io(server);
-//io-connection, server-client
-io.on("connection", (socket)=>{
-    //listen on chat_room
-    socket.on("chat_room", (msg)=>{
-        //get msg, then emit to other users
-        socket.broadcast.emit("chat_room", msg);
+
+/**
+ * handle io
+ */
+var count = 0;
+//noinspection JSUnresolvedFunction
+io.on("connection", function(socket){
+    count++;
+    console.log(count + " connected");
+    socket.on("disconnect", function(){
+        count--;
+        console.log(count + " connected");
+    });
+
+    socket.on("chat_room", function(client_msg){
+        console.log("msg: " + client_msg);
+        //noinspection JSUnresolvedVariable
+        socket.broadcast.emit("chat_room", client_msg);
     });
 });
-
-
